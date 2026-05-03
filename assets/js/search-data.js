8e20a1726e0483718650fa0cb2e4adb6832da31d
guo-zhenran <guo.zhenran@outlook.com>
2026-05-03 02:58:32 +0000

    Deploying to gh-pages from @ guo-zhenran/guo-zhenran.github.io@90dfe070bacb9d0947eb1cd9c5dbfd44353a07bc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,7 @@ ninja.data = [{
           description: "Under Construction",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/publications/";
+            window.location.href = "/research/";
           },
         },{id: "nav-notes-amp-resources",
           title: "Notes &amp; Resources",
@@ -31,6 +31,27 @@ ninja.data = [{
             window.location.href = "/notes/";
           },
         },{
+        id: 'social-cv',
+        title: 'CV',
+        section: 'Socials',
+        handler: () => {
+          window.open("/assets/pdf/CV_guo-zhenran.pdf", "_blank");
+        },
+      },{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%6D%61%69%6C%74%6F:%67%75%6F.%7A%68%65%6E%72%61%6E@%6F%75%74%6C%6F%6F%6B.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
